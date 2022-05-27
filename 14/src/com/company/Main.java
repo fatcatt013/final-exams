40d94009e94a408ea56e78cde3ef61584335e19f
mallasorte12 <mallasorte12@gmail.com>
2022-05-27 12:24:43 +0200

    14 done

---
+++ 14/src/com/company/Main.java
@@ -0,0 +1,26 @@
+package com.company;
+
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    System.out.println(sifra("Jedu si takhle taborit"));
+    }
+
+    public static String sifra(String text){
+        String[] slova = text.split(" ");
+        String output = "";
+
+        for (int i = 0; i < slova.length; i++){
+            String temp = slova[i];
+            for (int y = 0; y < temp.length(); y++){
+                int tempChar = temp.charAt(y);
+                output += "*" + tempChar;
+            }
+            output += "* ";
+        }
+
+        return output;
+    }
+}
